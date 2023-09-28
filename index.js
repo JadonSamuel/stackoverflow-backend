0b51fe1393014dc88f5a73d0ceb31e7525890780
Jadon Samuel <jadonlsamuel@gmail.com>
2023-09-28 20:30:03 +0530

    index js updated

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import userRoutes from "./routes/users.js";
 import questionRoutes from "./routes/Questions.js";
 import answerRoutes from "./routes/Answers.js";
 import connectDB from "./connectMongoDb.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 connectDB();
@@ -14,15 +15,21 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-// app.use('/',(req, res) => {
-//     res.send("This is a stack overflow clone API")
-// })
+app.use('/',(req, res) => {
+     res.send("This is a stack overflow clone API")
+ })
 
 app.use("/user", userRoutes);
 app.use("/questions", questionRoutes);
 app.use("/answer", answerRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+const CONNECTION_URL = process.env.CONNECTION_URL
+
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true})
+   .then(()=> app.listen(PORT, ()=>{console.log('server running on port ${PORT}')}))
+   .catch((err)=> console.log(err.message))
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
